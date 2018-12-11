648d0d70f43ca7ac97d1bdefdf640fe771e0337e
captainbowtie <allen@allenbarr.com>
2018-12-11 21:14:11 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+res.json({msg: "node works"});
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Server running on port ${port}`));
